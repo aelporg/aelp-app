cb1d5adf33a21f5a89f332a51084cfcdf711185a
Talha Balaj <talhabalaj@hotmail.com>
2021-10-22 09:17:07 +0500

    Added frontend, reset API

--- apps/api/src/app/submodules/auth.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import AuthResolver from './auth.resolver';
-
-@Module({ providers: [AuthResolver] })
-export default class AuthModule {}
--- apps/api/src/app/submodules/user.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserCrudResolver } from '.generated/graphql';
-
-@Module({ providers: [UserCrudResolver] })
-export default class UserModule {}
