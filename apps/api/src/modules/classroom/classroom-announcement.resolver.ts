00d4e8d43da492de546cea9d5ed588631e2e56e8
Talha Balaj <talhabalaj@hotmail.com>
2022-02-21 06:01:11 +0000

    Add createAnnouncement mutation

--- apps/api/src/modules/classroom/classroom-announcement.resolver.ts
+++ apps/api/src/modules/classroom/classroom-announcement.resolver.ts
@@ -1,13 +1,28 @@
-import { Parent, ResolveField, Resolver } from "@nestjs/graphql";
-import ClassroomAnnouncementsService from "./classroom-announcement.service";
-import { ClassroomAnnouncement } from "./types/classroom-announcement.model";
+import { Args, Mutation, Parent, ResolveField, Resolver } from '@nestjs/graphql'
+import { LoggedInUser } from '../../utils/decorators/LoggedInUser'
+import { User } from '../user/types/user.model'
+import ClassroomAnnouncementsService from './classroom-announcement.service'
+import { ClassroomAnnouncement } from './types/classroom-announcement.model'
+import { ClassroomAnnoucementCreateInput } from './types/create-announcement-input-type'
 
 @Resolver(() => ClassroomAnnouncement)
 export default class ClassroomAnnouncementResolver {
-  constructor (private classAnnoceService: ClassroomAnnouncementsService) {}
+  constructor(
+    private classroomAnnouncementService: ClassroomAnnouncementsService
+  ) {}
 
   @ResolveField()
   async attachments(@Parent() annoucement: ClassroomAnnouncement) {
-    return this.classAnnoceService.getAnnoucementAttachments(annoucement.id)
+    return this.classroomAnnouncementService.getAnnoucementAttachments(
+      annoucement.id
+    )
+  }
+
+  @Mutation(() => ClassroomAnnouncement)
+  async createAnnouncement(
+    @Args('data') data: ClassroomAnnoucementCreateInput,
+    @LoggedInUser() user: User
+  ) {
+    return this.classroomAnnouncementService.createAnnoucement(data, user)
   }
 }
