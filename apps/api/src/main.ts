36c6716c9bd07c9e4dc9f0fd7c577752b79dfa47
Talha Balaj <talhabalaj@hotmail.com>
2022-03-05 16:43:48 +0000

    Trigger change

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common'
 import { JoiPipe } from 'nestjs-joi'
 import { NestFactory } from '@nestjs/core'
