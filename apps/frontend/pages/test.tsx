049012bcb2b60a60eeec34e5966daf4f9d07a7e7
Talha Balaj <talhabalaj@hotmail.com>
2022-03-24 04:42:24 +0000

    fix(frontend): fix

--- apps/frontend/pages/test.tsx
+++ apps/frontend/pages/test.tsx
@@ -1,21 +1,17 @@
 import Button from '@components/primitives/button'
-import ScreenCenter from '@components/primitives/screen-center'
 import { useRef } from 'react'
 import { toast } from 'react-toastify'
-
 
 export default function Test() {
   const count = useRef(0)
 
   return (
-    <ScreenCenter>
-      <Button
-        onClick={() => {
-          toast.error("Hello world!")
-        }}
-      >
-        Show toast
-      </Button>
-    </ScreenCenter>
+    <Button
+      onClick={() => {
+        toast.error('Hello world!')
+      }}
+    >
+      Show toast
+    </Button>
   )
 }
